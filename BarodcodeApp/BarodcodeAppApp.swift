3d1dcb4c04ed7934923982ad2fa5cff3301df72f
Sambhav Singh <ryugasingh90@gmail.com>
2024-10-01 14:58:35 +0530

    Initial Commit

---
+++ BarodcodeApp/BarodcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarodcodeAppApp.swift
+//  BarodcodeApp
+//
+//  Created by Sambhav Singh on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BarodcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarodcodeAppTests/BarodcodeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarodcodeAppTests.swift
+//  BarodcodeAppTests
+//
+//  Created by Sambhav Singh on 01/10/24.
+//
+
+import Testing
+@testable import BarodcodeApp
+
+struct BarodcodeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
