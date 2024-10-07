b9b004194d8725d0e8216812bfb3ca5eee2b9050
Sambhav Singh <ryugasingh90@gmail.com>
2024-10-07 17:35:37 +0530

    half of the Ui is done only few parts are left

--- BarodcodeApp/BarCode.swift
+++ BarodcodeApp/BarCode.swift
@@ -7,18 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BarCode: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    BarCode()
 }
